9c94141206cbb793e5d0150a66b09b010ae79b17
IsmaMG18 <ismaelmg46@gmail.com>
2024-04-03 11:40:28 -0600

    Cambios en caratula y ficha de datos

--- routes/reportes.php
+++ routes/reportes.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\Reportes\Reporte;
-use Illuminate\Support\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +19,9 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get("/informe_de_inicio/{id}", function ($id) {
         $reporte = Reporte::findOrFail($id);
         return Pdf::loadView("reportes.informe_de_inicio", ["reporte" => $reporte])->stream($reporte->folio.".pdf");
+    });
+
+    Route::get("/ficha_de_datos", function () {
+        return Pdf::loadView("reportes.ficha_de_datos")->stream();
     });
 });
